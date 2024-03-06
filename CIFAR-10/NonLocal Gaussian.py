8819704cf38359d2a81079c7978993e20353b900
Dipkamal Bhusal <dip.kamal001@gmail.com>
2024-03-06 16:55:25 -0500

    file added

---
+++ CIFAR-10/NonLocal Gaussian.py
@@ -0,0 +1,400 @@
+
+
+import os
+import shutil
+import time
+import gc
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.autograd import Variable
+import math
+import kornia
+from cleverhans.torch.attacks.projected_gradient_descent import (
+    projected_gradient_descent,
+)
+from metrics import *
+
+eps = 0.01
+attack_steps = 7 
+step_size = 0.0025
+random_start= True
+
+batch_size = 128
+lr = 0.1
+momentum = 0.9
+weight_decay = 5e-4
+layers = 28
+widen_factor = 10
+droprate = 0
+nesterov = True
+max_num_training_steps = 70001
+mile1 = 40000 
+mile2 = 50000
+eps = 0.0314
+attack_steps = 7 
+step_size = 0.00784
+random_start= True
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+"""# using wide residual network"""
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, stride, dropRate=0.0):
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.droprate = dropRate
+        self.equalInOut = (in_planes == out_planes)
+        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
+                               padding=0, bias=False) or None
+    def forward(self, x):
+        if not self.equalInOut:
+            x = self.relu1(self.bn1(x))
+        else:
+            out = self.relu1(self.bn1(x))
+        out = self.relu2(self.bn2(self.conv1(out if self.equalInOut else x)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, training=self.training)
+        out = self.conv2(out)
+        return torch.add(x if self.equalInOut else self.convShortcut(x), out)
+
+class NetworkBlock(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, block, stride, dropRate=0.0):
+        super(NetworkBlock, self).__init__()
+        self.layer = self._make_layer(block, in_planes, out_planes, nb_layers, stride, dropRate)
+    def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, dropRate):
+        layers = []
+        for i in range(int(nb_layers)):
+            layers.append(block(i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, dropRate))
+        return nn.Sequential(*layers)
+    def forward(self, x):
+        return self.layer(x)
+
+
+
+class filter_denoising_block(torch.nn.Module):
+    ''' Simple filters as denoising block'''
+    def __init__(self, in_planes, ksize, filter_type):
+        super(filter_denoising_block, self).__init__()
+        self.in_planes = in_planes
+        self.ksize = ksize
+        self.filter_type = filter_type
+        self.conv = nn.Conv2d(in_channels=in_planes, out_channels=in_planes, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        if self.filter_type == 'Median':
+            x_denoised = kornia.filters.median_blur(x, (self.ksize, self.ksize))
+        elif self.filter_type == 'Mean':
+            x_denoised = kornia.filters.box_blur(x, (self.ksize, self.ksize))
+        elif self.filter_type == 'Gaussian':
+            x_denoised = kornia.filters.gaussian_blur2d(x, (self.ksize, self.ksize), (0.3 * ((x.shape[3] - 1) * 0.5 - 1) + 0.8, 0.3 * ((x.shape[2] - 1) * 0.5 - 1) + 0.8))
+        new_x = x + self.conv(x_denoised)
+        return new_x
+
+
+class non_local_denoising_block(torch.nn.Module):
+    def __init__(self, in_channels, inter_channels = None, mode = 'nonlocal_gaussian', bn_layer = True):
+
+        """ in_channels: original channel size (1024 in the paper) #depends on the test dataset
+            inter_channels: channel size inside the block if not specifed reduced to half (512 in the paper)
+            mode: supports Gaussian, Embedded Gaussian, Dot Product, and Concatenation
+            dimension: can be 1 (temporal), 2 (spatial), 3 (spatiotemporal)
+            bn_layer: whether to add batch norm
+            """
+
+        super().__init__()
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+        self.mode = mode
+        self.inter_channels = inter_channels
+
+         # the channel size is reduced to half inside the block
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+
+        #dimension=2 for images
+        conv_nd = nn.Conv2d
+        max_pool_layer = nn.MaxPool2d(kernel_size=(2,2))
+        bn = nn.BatchNorm2d
+
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+
+        # add BatchNorm layer after the last conv layer
+        if bn_layer:
+            self.W_z = nn.Sequential(
+                    conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels, kernel_size=1),
+                    bn(self.in_channels)
+                )
+            # from section 4.1 of the paper, initializing params of BN ensures that the initial state of non-local block is identity mapping
+            nn.init.constant_(self.W_z[1].weight, 0)
+            nn.init.constant_(self.W_z[1].bias, 0)
+        else:
+            self.W_z = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels, kernel_size=1)
+
+            # from section 3.3 of the paper by initializing Wz to 0, this block can be inserted to any existing architecture
+            nn.init.constant_(self.W_z.weight, 0)
+            nn.init.constant_(self.W_z.bias, 0)
+
+
+        # define theta and phi for all operations except gaussian
+        if self.mode == "embedded" or self.mode == "dot" or self.mode == "concatenate":
+            self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+            self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+
+        if self.mode == "concatenate":
+            self.W_f = nn.Sequential(
+                    nn.Conv2d(in_channels=self.inter_channels * 2, out_channels=1, kernel_size=1),
+                    nn.ReLU()
+                )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        if self.mode == "nonlocal_gaussian":
+            theta_x = x.view(batch_size, self.in_channels, -1)
+            phi_x = x.view(batch_size, self.in_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            f = torch.matmul(theta_x, phi_x)
+
+        elif self.mode == "embedded" or self.mode == "dot":
+            theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+            phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            f = torch.matmul(theta_x, phi_x)
+
+        elif self.mode == "concatenate":
+            theta_x = self.theta(x).view(batch_size, self.inter_channels, -1, 1)
+            phi_x = self.phi(x).view(batch_size, self.inter_channels, 1, -1)
+
+            h = theta_x.size(2)
+            w = phi_x.size(3)
+            theta_x = theta_x.repeat(1, 1, 1, w)
+            phi_x = phi_x.repeat(1, 1, h, 1)
+
+            concat = torch.cat([theta_x, phi_x], dim=1)
+            f = self.W_f(concat)
+            f = f.view(f.size(0), f.size(2), f.size(3))
+
+        if self.mode == "nonlocal_gaussian" or self.mode == "embedded":
+            f_div_C = F.softmax(f, dim=-1)
+        elif self.mode == "dot" or self.mode == "concatenate":
+            N = f.size(-1) # number of position in x
+            f_div_C = f / N
+
+        y = torch.matmul(f_div_C, g_x)
+
+        # contiguous here just allocates contiguous chunk of memory
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
+
+        W_y = self.W_z(y)
+        # residual connection
+        z = W_y + x
+
+        return z
+
+
+
+
+class WideResNet(nn.Module):
+    def __init__(self, depth, num_classes, widen_factor=1, dropRate=0.0):
+        super(WideResNet, self).__init__()
+        nChannels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
+        assert ((depth - 4) % 6 == 0)
+        n = (depth - 4) / 6
+        block = BasicBlock
+
+        # 1st conv before any network block
+        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1, padding=1, bias=False)
+
+        # 1st block
+        self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1, dropRate)
+
+        # Denoising block
+        # self.denoise_block = filter_denoising_block(nChannels[1], ksize=3, filter_type='Median')
+        self.non_local_denoise = non_local_denoising_block(in_channels=nChannels[1], inter_channels=nChannels[1] // 2, mode="nonlocal_gaussian", bn_layer=True)
+
+        # 2nd block
+        self.block2 = NetworkBlock(n, nChannels[1], nChannels[2], block, 2, dropRate)
+
+        # 3rd block
+        self.block3 = NetworkBlock(n, nChannels[2], nChannels[3], block, 2, dropRate)
+
+        # global average pooling and classifier
+        self.bn1 = nn.BatchNorm2d(nChannels[3])
+        self.relu = nn.ReLU(inplace=True)
+        self.fc = nn.Linear(nChannels[3], num_classes)
+        self.nChannels = nChannels[3]
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.block1(out)
+
+        # Apply the denoising block
+        out = self.non_local_denoise(out)
+
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        return self.fc(out)
+
+
+
+normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x/255.0 for x in [63.0, 62.1, 66.7]])
+
+transform_train = transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+            ])
+transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+        ])
+
+train_loader = torch.utils.data.DataLoader(
+        datasets.__dict__['CIFAR10']('../data', train=True, download=True,
+                         transform=transform_train),
+        batch_size=batch_size, shuffle=True)
+
+val_loader = torch.utils.data.DataLoader(
+        datasets.__dict__['CIFAR10']('../data', train=False, transform=transform_test),
+        batch_size=32, shuffle=True)
+
+def evaluate_model(model, data, device):
+    """Evaluate torch model."""
+    model.eval()
+    logits = torch.Tensor().to(device)
+    targets = torch.LongTensor().to(device)
+
+    with torch.no_grad():
+        for images, labels in data:
+            images, labels = images.to(device), labels.to(device)
+            logits = torch.cat([logits, model(images)])
+            targets = torch.cat([targets, labels])
+
+    predictions, labels = torch.nn.functional.softmax(logits, dim=1), targets
+    test_acc = np.mean(np.argmax(predictions.cpu().numpy(), axis=1) == labels.cpu().numpy())
+    return test_acc
+
+
+
+def train(train_loader, model, criterion, optimizer, scheduler, epoch):
+    """Train for one epoch on the training set"""
+
+    # switch to train mode
+    model.train()
+    for i, (input, target) in enumerate(train_loader):
+        target = target.cuda(non_blocking=True)
+        input = input.cuda(non_blocking=True)
+
+        # compute output
+        output = model(input)
+        loss = criterion(output, target)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+
+def evaluate_model(model, data, device):
+    """Evaluate torch model."""
+    model.eval()
+    logits = torch.Tensor().to(device)
+    targets = torch.LongTensor().to(device)
+
+    with torch.no_grad():
+        for images, labels in data:
+            images, labels = images.to(device), labels.to(device)
+            logits = torch.cat([logits, model(images)])
+            targets = torch.cat([targets, labels])
+
+    predictions, labels = torch.nn.functional.softmax(logits, dim=1), targets
+    test_acc = np.mean(np.argmax(predictions.cpu().numpy(), axis=1) == labels.cpu().numpy())
+    return test_acc
+
+
+#adv training
+
+model = WideResNet(layers, 10 , widen_factor, dropRate=droprate)
+model = model.cuda()
+
+criterion = nn.CrossEntropyLoss().cuda()
+optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum, nesterov = nesterov,
+                                weight_decay=weight_decay)
+
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[mile1, mile2], gamma=0.1)
+
+print('non local gaussian adv first layer')
+
+
+for step in range(0, max_num_training_steps):
+
+    model.train()
+    input, target = next(iter(train_loader))
+    target = target.cuda(non_blocking=True)
+    input = input.cuda(non_blocking=True)
+    images = projected_gradient_descent(model, input, eps, step_size, attack_steps,np.inf)
+    optimizer.zero_grad()
+
+    # compute output
+    output = model(images)
+    loss = criterion(output, target)
+
+    # compute gradient and do SGD step
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+
+    if step%500==0:
+        print('step ', step)
+        print(evaluate_model(model, val_loader, device))
+
+
+
+
+path = 'Layer_1_Eps_D_nonlocal_gaussian_advCIFARWideResNet.pth'
+torch.save(model.state_dict(), path)
+
+
+gc.collect()
+torch.cuda.empty_cache()
+
+del model
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ImprovingVG
+# On the connection between model robustness and saliency map interpretability
+
+Repository for the paper "On the connection between model robustness and saliency map interpretability," submitted at The 18th European Conference on Computer Vision ECCV 2024.
+
+This paper provides our implementation of building several robust models for improving the quality of saliency maps using Vanilla Gradient method. You will need to download ImageNette dataset from [here](https://github.com/fastai/imagenette). 
+
+## Requirement
+- kornia (for applying filters to feature-maps)
+- cleverhans (for adversarial training)
+- captum (for saliency maps)
+- quantus (for computing several metrics for saliency maps)
+
+
+The directory consists of the following file and folders:
+
+    FMNIST: This folder consists of a notebook that demonstrates training of several robust and non-robust FMNIST models. 
+    CIFAR-10: This folder consists of python files for training robust and non-robust CIFAR-10 models.
+    ImageNette: This folder consists of python files for training robust and non-robust ImageNette models.
+    metrics.py: This program file consists of python functions for evaluating saliency maps.
